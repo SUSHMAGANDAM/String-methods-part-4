9cad62e4500710fccb34da2ab5398f45bce649b0
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-02-14 15:05:51 +0530

    Add files via upload

---
+++ string method part 4.py
@@ -0,0 +1,8 @@
+# s=[int(x) for x in input().split()]
+# print(s,type(s))
+# print(s[0]+s[1]+s[2])
+# print(sum(s))
+# print("the sum is"+" "+str(sum(s)))
+# print("the sum is",sum(s))
+h="hello welcome to python programming language"
+print(h.split())
